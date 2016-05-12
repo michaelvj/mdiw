224b15ec88dcf65e23968779ddc5daa649f62c0a
Stagiair <michaelvanjelgerhuis@gmail.com>
2016-05-12 17:48:03 +0200

    Updated routing functionality

--- src/app.js
+++ src/app.js
@@ -10,29 +10,39 @@ var Manipulare = angular.module('Manipulare', ['ui.router']);
 
 
 Manipulare.config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $urlRouterProvider) {
-    $urlRouterProvider.otherwise('/list');
+    $urlRouterProvider.when('/', '/home/app');
+    $urlRouterProvider.when('/home', '/home/app');
+    $urlRouterProvider.when('/querying', 'home/app/querying');
+    $urlRouterProvider.when('/list', 'home/app/list');
+    $urlRouterProvider.when('/compconfig', 'home/app/compconfig');
 
     $stateProvider
-    .state('app', {
-        url: '',
+    .state('parent', {
+        url: '/home',
         abstract: true,
-        templateUrl: 'app.html'
+        template: '<ui-view/>'
     })
-    .state('app.querying', {
+
+    .state('parent.app', {
+        url: '/app',
+        templateUrl: '/app/app.html'
+    })
+
+    .state('parent.app.querying', {
         url: '/querying',
         templateUrl: '/app/querying/querying.html',
         controller: 'queryingCtrl',
         controllerAs: 'vm'
     })
 
-    .state('app.compconfig', {
+    .state('parent.app.compconfig', {
         url: '/compconfig',
         templateUrl: '/app/compconfig/compconfig.html',
         controller: 'compconfigCtrl',
         controllerAs: 'vm'
     })
 
-    .state('app.list', {
+    .state('parent.app.list', {
         url: '/list',
         templateUrl: '/app/list/list.html',
         controller: 'listCtrl',
